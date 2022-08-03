bae2614eced5f656b70e9b4109f8325e6ec6bc4a
habibmustafa <hebibullahmustafazade@gmail.com>
2022-08-03 23:51:00 +0400

    NavLink to Link

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import "./style.scss"
 
 const Button = ({ to, title }) => {
    return (
-      <NavLink className='get' to={to}>{title}</NavLink>
+      <Link className='get' to={to}>{title}</Link>
    )
 }
 
