b4aef200b71492cf96c378b2d9395b45651452a7
habibmustafa <hebibullahmustafazade@gmail.com>
2022-08-08 18:01:04 +0400

    Added submitted and loading button

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,30 +1,35 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import "./style.scss"
 import emailjs from "@emailjs/browser"
 import { Faq } from "../../data/Faq"
 import FaqItem from './components/FaqItem'
 
 const Contact = () => {
-   // const [selected, setSelected] = useState(null)
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [phone, setPhone] = useState('')
+   const [subject, setSubject] = useState('')
+   const [message, setMessage] = useState('')
+   const [submitButton, setSubmitButton] = useState(false)
+   const [sendMessage, setSendMessage] = useState(false)
    const form = useRef()
 
    const sendEmail = (e) => {
       e.preventDefault()
+
       emailjs.sendForm('service_mffkcm4', 'template_jfsdgtb', form.current, '8kOeDcVnJ0N6Cw-5t')
          .then((result) => {
             console.log(result.text);
+            setName('')
+            setEmail('')
+            setPhone('')
+            setSubject('')
+            setMessage('')
+            setSendMessage(true)
          }, (error) => {
             console.log(error.text);
          });
    }
-
-   // const toggle = i => {
-   //    if (selected === i) {
-   //       return setSelected(null)
-   //    }
-
-   //    setSelected(i)
-   // }
 
    return (
       <div className='contact'>
@@ -39,34 +44,43 @@ const Contact = () => {
                   </p>
                </div>
                <div className="contact-form" data-aos="fade-zoom-in" data-aos-delay="300">
-                  <form ref={form} onSubmit={sendEmail}>
-                     <div className='contact-form-grid'>
-                        <div className="input-wrapper">
-                           <label htmlFor="name">Name</label>
-                           <input placeholder='Full Name' type="text" name="name" id="name" required />
-                        </div>
-                        <div className="input-wrapper">
-                           <label htmlFor="email">Email Address</label>
-                           <input placeholder='contact@you.com' type="email" name="email" id="email" required />
-                        </div>
-                        <div className="input-wrapper">
-                           <label htmlFor="phone">Phone</label>
-                           <input placeholder='(+994) 51 600-2230' type="tel" name="phone" id="phone" required />
-                        </div>
-                        <div className="input-wrapper">
-                           <label htmlFor="subject">Subject</label>
-                           <input placeholder='Ex. New Project' type="text" name="subject" id="subject" required />
-                        </div>
+                  {!sendMessage ?
+                     <form ref={form} onSubmit={sendEmail}>
+                        <div className='contact-form-grid'>
+                           <div className="input-wrapper">
+                              <label htmlFor="name">Name</label>
+                              <input onChange={(e) => {setName(e.target.value)}} value={name} placeholder='Full Name' type="text" name="name" id="name" required />
+                           </div>
+                           <div className="input-wrapper">
+                              <label htmlFor="email">Email Address</label>
+                              <input onChange={(e) => {setEmail(e.target.value)}} value={email}  placeholder='contact@you.com' type="email" name="email" id="email" required />
+                           </div>
+                           <div className="input-wrapper">
+                              <label htmlFor="phone">Phone</label>
+                              <input onChange={(e) => {setPhone(e.target.value)}} value={phone}  placeholder='(+994) 51 600-2230' type="tel" name="phone" id="phone" required />
+                           </div>
+                           <div className="input-wrapper">
+                              <label htmlFor="subject">Subject</label>
+                              <input onChange={(e) => {setSubject(e.target.value)}} value={subject}  placeholder='Ex. New Project' type="text" name="subject" id="subject" required />
+                           </div>
 
-                        <div className="input-wrapper message">
-                           <label htmlFor="message">Message</label>
-                           <textarea placeholder="Please write your message..." name="message" id="message" rows="5" required></textarea>
+                           <div className="input-wrapper message">
+                              <label htmlFor="message">Message</label>
+                              <textarea onChange={(e) => {setMessage(e.target.value)}} value={message}  placeholder="Please write your message..." name="message" id="message" rows="5" required></textarea>
+                           </div>
+                           <div className="input-wrapper button">
+                              <input onClick={() => {setSubmitButton(true)}} type="submit" value={submitButton ? "Please wait..." : "Send Message"} data-wait="Please wait..." />
+                           </div>
                         </div>
-                        <div className="input-wrapper button">
-                           <input type="submit" value="Send Message" data-wait="Please wait..." />
-                        </div>
-                     </div>
-                  </form>
+                     </form> :
+                     <div className='submitted'>
+                        <h2 className='h2-size'>Your message has been submitted.</h2>
+                        <img src="https://assets.website-files.com/61129bb2ecedf803564c68ec/6112bdfb2b341387dfa368b4_portfolio-underline-personal-x-template.svg" alt="line" />
+                        <p className='paragraph'>
+                           I will get back to you within 24-48 hours. 
+                           elit, sed do eiusmod tempor incididunt ut.
+                        </p>
+                     </div>}
                </div>
 
                <div className='contact-social' data-aos="fade-zoom-in" data-aos-offset="100">
@@ -94,7 +108,7 @@ const Contact = () => {
 
                <div className="faq-items">
                   {Faq.map((item, i) => (
-                     <FaqItem key={i} item={item} i={i}/>
+                     <FaqItem key={i} item={item} i={i} />
                   ))}
 
                </div>
