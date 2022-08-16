1bc66874bc516d4626b41479b322a528e2df4943
habibmustafa <hebibullahmustafazade@gmail.com>
2022-08-16 11:44:37 +0400

    test

---
+++ src/data/Faq.js
@@ -0,0 +1,18 @@
+export const Faq = [
+   {
+      title: "Are you open for freelance / contract work?",
+      content: "Yeah, to be honest, I prefer contract work more at the moment. I believe more that contract work will increase my experience"
+   },
+   {
+      title: "Are you open for programmer collaborations?",
+      content: "Yes, I am ready to cooperate with other programmers. I think it will be good for me to develop new, modern projects together. Who knows, maybe we will lay the foundation of a very big business."
+   },
+   {
+      title: "Are you currently looking for full time roles?",
+      content: "No, because I am currently studying for a bachelor's degree. Considering that I spend half the day at the university, this is not possible. But, I am open to part time roles."
+   },
+   {
+      title: "What's your past experience on programming?",
+      content: "I have been trying to develop myself in the field of programming for the last 2 years. I have been actively working as a front-end developer for the past 6 months. I have entry level backend knowledge."
+   },
+]
---
+++ src/data/Skills.js
@@ -0,0 +1,17 @@
+export const Skills = [
+   {
+      title: "Design",
+      img: "https://assets.website-files.com/61129bb2ecedf803564c68ec/61140f8059c9332ddb393562_skill-icon-01-personal-x-template.svg",
+      item: ["UI/UX Design", "Product Design", "Web Design", "Design Thinking", "Branding"],
+   },
+   {
+      title: "Development",
+      img: "https://assets.website-files.com/61129bb2ecedf803564c68ec/61140f802d167b3412c5a3d0_skill-icon-02-personal-x-template.svg",
+      item: ["HTML & CSS", "Javascript", "React", "React Native", "No Code"],
+   },
+   {
+      title: "Tools",
+      img: "https://assets.website-files.com/61129bb2ecedf803564c68ec/61140f8020e63656704c6b6d_skill-icon-03-personal-x-template.svg",
+      item: ["Webflow", "Figma", "Notion", "Photoshop", "Illustrator"],
+   },
+]
