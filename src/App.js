ace453635e80d53dfe60c1b166776805c9e03fe3
habibmustafa <hebibullahmustafazade@gmail.com>
2022-08-17 17:32:46 +0400

    Added ScrollToTop

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AOS from 'aos'
+import React, { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import Header from "./components/Header"
-import './App.scss';
-import RoutesPage from './config/ReactRoutes';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import "./App.scss";
+import RoutesPage from "./config/ReactRoutes";
+import Footer from "./components/Footer";
+import ScrollToTop from "./config/ScrollToTop";
 
 function App() {
-
    // AOS animation
    useEffect(() => {
       AOS.init({
          useClassNames: true,
-         easing: 'ease-in-back',
+         easing: "ease-in-back",
          delay: 0,
          offset: 250,
          once: true,
          mirror: true,
-         duration: 500
+         duration: 500,
       });
 
       AOS.refresh();
    }, []);
 
-   
    return (
       <BrowserRouter>
          <>
+            <ScrollToTop />
             <Header />
             <RoutesPage />
             <Footer />
